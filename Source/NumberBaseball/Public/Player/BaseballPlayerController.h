0d4b6efa937407c8a8799944d897d64d215de166
DevKolIn <donghyeon.study@gmail.com>
2025-03-20 11:12:18 +0900

    [feat] Unreal 기본 클래스 생성

---
+++ Source/NumberBaseball/Public/Player/BaseballPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BaseballPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NUMBERBASEBALL_API ABaseballPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
